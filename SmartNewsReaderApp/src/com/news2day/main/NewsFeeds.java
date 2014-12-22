582c07ba240388ea9b3b817f937192259849db48
abhishek15101989 <abhishek15.10.1989@gmail.com>
2014-12-22 18:45:47 -0500

    news details commit

--- SmartNewsReaderApp/src/com/news2day/main/NewsFeeds.java
+++ SmartNewsReaderApp/src/com/news2day/main/NewsFeeds.java
@@ -1,6 +1,11 @@
 package com.news2day.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +15,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup; 
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -18,64 +23,63 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.news2day.R;
+import com.news2day.database.DatabaseHelper;
 import com.news2day.preference.Preferences;
 
 public class NewsFeeds extends ActionBarActivity {
-	 ArrayList<String> newsFeedsList;
+	ArrayList<String> newsFeedsList = new ArrayList<String>();
+	HashMap<String, JSONObject> hashMap = new HashMap<String, JSONObject>();
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.news_feed_list);
-		ListView newsList=(ListView)findViewById(R.id.news_list);
-		newsFeedsList = new ArrayList<String>();
-         getnewsFeedsList();
-         // Create The Adapter with passing ArrayList as 3rd parameter
-         ArrayAdapter<String> arrayAdapter =      
-         new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, newsFeedsList);
-         // Set The Adapter
-         newsList.setAdapter(arrayAdapter); 
-         
-         // register onClickListener to handle click events on each item
-         newsList.setOnItemClickListener(new OnItemClickListener()
-            {
-                     // argument position gives the index of item which is clicked
-					@Override
-					public void onItemClick(AdapterView<?> parent, View view,
-							int position, long id) {
-						Toast.makeText(getApplicationContext(), "Animal Selected : "+position,   Toast.LENGTH_LONG).show();
-					}
-            });
-		  
+		ListView newsList = (ListView) findViewById(R.id.news_list);
+		DatabaseHelper dbhelper = new DatabaseHelper(this);
+		
+		JSONArray array = dbhelper.getNewsItemsForTitle("New York Times");
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject json;
+			try {
+				json = array.getJSONObject(i);
+				hashMap.put(json.getString("title"), json);
+				newsFeedsList.add(json.getString("title"));
+			}catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, newsFeedsList);
+		newsList.setAdapter(arrayAdapter);
+		newsList.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {	
+				startDetailActivity(arrayAdapter,position);
+				
+			}
+		});
+
 	}
-    void getnewsFeedsList()
-    {
-    	newsFeedsList.add("DOG");
-    	newsFeedsList.add("CAT");
-    	newsFeedsList.add("HORSE");
-    	newsFeedsList.add("ELEPHANT");
-    	newsFeedsList.add("LION");
-    	newsFeedsList.add("COW");
-        newsFeedsList.add("MONKEY");
-        newsFeedsList.add("DEER");
-        newsFeedsList.add("RABBIT");
-        newsFeedsList.add("BEER");
-        newsFeedsList.add("DONKEY");
-        newsFeedsList.add("LAMB");
-        newsFeedsList.add("GOAT");
-        
-    }
+	public void startDetailActivity(ArrayAdapter<String> arrayAdapter, int position){
+		JSONObject json = hashMap.get(arrayAdapter.getItem(position));
+		Intent intent = new Intent();
+		intent.putExtra("json", json.toString());
+		startActivity(new Intent(this, News_Detail_Activity.class));
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		 //Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.news_feed_menu, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-//		 Handle action bar item clicks here. The action bar will
-//		 automatically handle clicks on the Home/Up button, so long
-//		 as you specify a parent activity in AndroidManifest.xml
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml
 
 		switch (item.getItemId()) {
 		case R.id.aboutUs:
