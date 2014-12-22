1d3277e0c64d0164f01348d27370871951f25c80
abhishek15101989 <abhishek15.10.1989@gmail.com>
2014-12-22 13:30:13 -0500

    commit

--- SmartNewsReaderApp/src/com/news2day/preference/Preferences.java
+++ SmartNewsReaderApp/src/com/news2day/preference/Preferences.java
@@ -1,14 +1,18 @@
 package com.news2day.preference;
 
+import com.news2day.R;
+
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class Preferences extends PreferenceActivity{
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
 	}
 	
 }
