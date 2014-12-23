2f613d523fd2c983f224618fdfb5c1dcb495c196
rahulkr09 <rk2375@nyu.edu>
2014-12-23 18:29:37 -0500

    Final App

--- SmartNewsReaderApp/src/com/news2day/preference/Preferences.java
+++ SmartNewsReaderApp/src/com/news2day/preference/Preferences.java
@@ -2,8 +2,12 @@ package com.news2day.preference;
 
 import com.news2day.R;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.View;
 
 public class Preferences extends PreferenceActivity{
 
@@ -13,6 +17,12 @@ public class Preferences extends PreferenceActivity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		   View view = new View(this);
+		   
+		   SharedPreferences prefs = PreferenceManager
+				    .getDefaultSharedPreferences(Preferences.this);
 	}
+	
 	
 }
